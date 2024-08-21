6d4fb16e7fcbd8c492c8399ea47efd8dc04a4c0d
luh000 <126384271+luh000@users.noreply.github.com>
2024-08-21 13:39:07 +1000

    Update signupScreen.dart

--- lib/screens/auth/signupScreen.dart
+++ lib/screens/auth/signupScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:csit998_capstone_g16/utils/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SignupScreen extends StatefulWidget {
